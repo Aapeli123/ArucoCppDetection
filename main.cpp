45a806c46f198da4178e2df94d91fe6aa9e04bac
Aapo Harju <aapo.harju@gmail.com>
2021-07-10 21:26:48 +0300

    Added aruco detection

--- main.cpp
+++ main.cpp
@@ -2,16 +2,45 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/mat.hpp>
+#include <opencv2/aruco.hpp>
+
+#include <vector>
 
 int main() {
-    cv::namedWindow("videoCapture", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("frame", cv::WINDOW_AUTOSIZE);
     auto capture = cv::VideoCapture(0);
     auto data = cv::Mat();
     
-    bool succ = capture.read(data);
-    if(!succ) {
-        std::cout << "Something went wrong...\n";
-        return 1;
-    }
-    cv::imshow("videoCapture", data);
+    // Aruco data
+    std::vector<int> ids;
+    std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+
+    // Aruco detection params
+    auto parameters = cv::aruco::DetectorParameters::create();
+    auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+
+
+    while(true) {
+        bool succ = capture.read(data);
+        if(!succ) {
+            std::cout << "Something went wrong...\n";
+            return 1;
+        }
+
+        // Detect arucos from data:
+        cv::aruco::detectMarkers(data, dictionary, markerCorners, ids, parameters);
+
+        // Create a clone image that we can write the locations of arucos on:
+        auto output = data.clone();
+        cv::aruco::drawDetectedMarkers(output, markerCorners, ids);
+
+        // Show image
+
+        cv::imshow("frame", output);
+        if(cv::waitKey(1) % 256 == 27) {
+            break;
+        }
+        std::cout << "Found " << ids.size() << " arucos\n";
+    };
+    return 0;
 }
